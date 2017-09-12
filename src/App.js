51ff71667ccfe3daced3194e2467339cea68bde5
Pavel Struhar <pstruhar@gmail.com>
2017-09-12 18:13:00 +0200

    Moving the robot with arrow keys

--- src/App.js
+++ src/App.js
@@ -1,21 +1,101 @@
 import React, { Component } from 'react';
+import { HotKeys } from 'react-hotkeys';
 import logo from './logo.svg';
 import './App.css';
 import MapStage from './MapStage';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      keys: {
+        up: false,
+        down: false,
+        left: false,
+        right: false,
+      }
+    }
+  }
+
+  handleKeyDown = (event) => {
+    event.preventDefault();
+    switch (event.key) {
+      case "ArrowDown":
+        this.setState((state) => ({...state, keys: {...state.keys, down: true}}));
+        break;
+      case "ArrowUp":
+        this.setState((state) => ({...state, keys: {...state.keys, up: true}}));
+        break;
+      case "ArrowLeft":
+        this.setState((state) => ({...state, keys: {...state.keys, left: true}}));
+        break;
+      case "ArrowRight":
+        this.setState((state) => ({...state, keys: {...state.keys, right: true}}));
+        break;
+      default:
+    }
+  }
+
+  handleKeyUp = (event) => {
+    event.preventDefault();
+    switch (event.key) {
+      case "ArrowDown":
+        this.setState((state) => ({...state, keys: {...state.keys, down: false}}));
+        break;
+      case "ArrowUp":
+        this.setState((state) => ({...state, keys: {...state.keys, up: false}}));
+        break;
+      case "ArrowLeft":
+        this.setState((state) => ({...state, keys: {...state.keys, left: false}}));  
+        break;
+      case "ArrowRight":
+        this.setState((state) => ({...state, keys: {...state.keys, right: false}}));
+        break;
+      default:
+    }
+  }
+
+  componentDidMount() {
+    this.wrapper.focus();
+  }
+
   render() {
+    const keyMap = {
+      arrowKeyDown: {
+        sequence: ['down', 'up', 'left', 'right'],
+        action: 'keydown'
+      },
+      arrowKeyUp: {
+        sequence: ['down', 'up', 'left', 'right'],
+        action: 'keyup'
+      },  
+    }
+    
+    const handlers = {
+      arrowKeyDown: this.handleKeyDown,
+      arrowKeyUp: this.handleKeyUp
+    };
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <HotKeys
+        keyMap={keyMap}
+        handlers={handlers}
+      >
+        <div
+          tabIndex={0}
+          ref={(wrapper) => { this.wrapper = wrapper; }}
+          className="App"
+        >
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React with Pixi.js (react-pixi)</h2>
+          </div>
+          <p className="App-intro">
+            Use arrows on your keyboard to move the robot around.
+          </p>
+          <MapStage keys={this.state.keys}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MapStage />
-      </div>
+      </HotKeys>
     );
   }
 }
--- src/MapStage.js
+++ src/MapStage.js
@@ -10,24 +10,71 @@ var Stage = ReactPIXI.Stage;
 var Sprite = ReactPIXI.Sprite;
 var TilingSprite = ReactPIXI.TilingSprite;
 
+const ROBOT_SPEED = 300/1000; // px per ms
+
 class MapStage extends Component {
   constructor() {
     super();
     this.state = {
+      lastAnimationTimestamp: null,
       robot: {
         x: 300,
-        y: 100,
-      }
+        y: 200,
+      },
+      currentFramerate: 0,
+      animationRequestID: null, // use it to cancel animation; not really necessary
     }
   }
 
+  animationCallback = (time) => {
+    this.setState(
+      (state) => {
+        const timeDifference = (time - state.lastAnimationTimestamp);
+        const distanceDifference = ROBOT_SPEED * timeDifference;
+        const newX = Math.round(
+          state.robot.x +
+          (this.props.keys.right ? 1 : 0) * distanceDifference +
+          (this.props.keys.left ? -1 : 0) * distanceDifference
+        );
+        const newY = Math.round(
+          state.robot.y +
+          (this.props.keys.down ? 1 : 0) * distanceDifference +
+          (this.props.keys.up ? -1 : 0) * distanceDifference              
+        );
+        const newFramerate = Math.round(1000/timeDifference);
+        return {
+          ...state,
+          animationRequestID: requestAnimationFrame(this.animationCallback),
+          robot: {
+            ...state.robot,
+            x: newX,
+            y: newY,
+          },
+          currentFramerate: newFramerate,
+          lastAnimationTimestamp: time,  
+        }            
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.setState((state) => ({
+      ...state,
+      animationRequestID: requestAnimationFrame(this.animationCallback),
+    }))
+  }
+
   render() {
-    const text = `Current framerate: ? fps`;
+    const text = `Current framerate: ${this.state.currentFramerate} fps`;
     return (
       <div>
         <Stage width={900} height={600}>
           <TilingSprite image={tileImg} width={900} height={600} key="tile" />
-          <Text text={text} x={150} y={0} anchor={new PIXI.Point(0.5,0)} key="2" />
+          <Text text={text} x={10} y={10} key="2" />
+          <Text text={`${this.props.keys.up ? 'up' : ''}`} x={10} y={40} anchor={new PIXI.Point(0,0)} key="3" />
+          <Text text={`${this.props.keys.down ? 'down' : ''}`} x={10} y={70} anchor={new PIXI.Point(0,0)} key="4" />
+          <Text text={`${this.props.keys.left ? 'left' : ''}`} x={10} y={100} anchor={new PIXI.Point(0,0)} key="5" />
+          <Text text={`${this.props.keys.right ? 'right' : ''}`} x={10} y={130} anchor={new PIXI.Point(0,0)} key="6" />
           <Sprite image={robotImg} scale={0.5} x={this.state.robot.x} y={this.state.robot.y} anchor={new PIXI.Point(0.5,0.5)} key="robot" />
         </Stage>
       </div>
