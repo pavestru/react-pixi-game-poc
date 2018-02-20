413b06dd385adf88fac984c7125f5263949d58c3
Pavel Struhar <pstruhar@gmail.com>
2018-02-20 02:07:30 +0100

    Upgrade to React 16 and react-pixi-fiber

--- src/MapStage.js
+++ src/MapStage.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import robotImg from './assets/robot.png';
 import tileImg from './assets/mars-surface-tile.gif';
 
-var ReactPIXI = require('react-pixi');
-var PIXI = require('pixi.js');
-var Text = ReactPIXI.Text;
-
-var Stage = ReactPIXI.Stage;
-var Sprite = ReactPIXI.Sprite;
-var TilingSprite = ReactPIXI.TilingSprite;
+import * as PIXI from 'pixi.js';
+import {
+  Sprite,
+  Stage,
+  Text,
+  TilingSprite,
+} from 'react-pixi-fiber';
 
 const ROBOT_SPEED = 300/1000; // px per ms
 
@@ -27,34 +27,56 @@ class MapStage extends Component {
   }
 
   animationCallback = (time) => {
-    this.setState(
-      (state) => {
-        const timeDifference = (time - state.lastAnimationTimestamp);
-        const distanceDifference = ROBOT_SPEED * timeDifference;
-        const newX = Math.round(
-          state.robot.x +
-          (this.props.keys.right ? 1 : 0) * distanceDifference +
-          (this.props.keys.left ? -1 : 0) * distanceDifference
-        );
-        const newY = Math.round(
-          state.robot.y +
-          (this.props.keys.down ? 1 : 0) * distanceDifference +
-          (this.props.keys.up ? -1 : 0) * distanceDifference              
-        );
-        const newFramerate = Math.round(1000/timeDifference);
-        return {
-          ...state,
-          animationRequestID: requestAnimationFrame(this.animationCallback),
-          robot: {
-            ...state.robot,
-            x: newX,
-            y: newY,
-          },
-          currentFramerate: newFramerate,
-          lastAnimationTimestamp: time,  
-        }            
-      }
-    );
+    const {
+      left,
+      right,
+      up,
+      down
+    } = this.props.keys;
+
+    if (left || right || up || down) {
+      this.setState(
+        (state) => {
+          const timeDifference = time - state.lastAnimationTimestamp;
+          const distanceDifference = ROBOT_SPEED * timeDifference;
+          const newX = Math.round(
+            state.robot.x +
+            (right ? 1 : 0) * distanceDifference +
+            (left ? -1 : 0) * distanceDifference
+          );
+          const newY = Math.round(
+            state.robot.y +
+            (down ? 1 : 0) * distanceDifference +
+            (up ? -1 : 0) * distanceDifference              
+          );
+          const newFramerate = Math.round(1000/timeDifference);
+          return {
+            ...state,
+            animationRequestID: requestAnimationFrame(this.animationCallback),
+            robot: {
+              ...state.robot,
+              x: newX,
+              y: newY,
+            },
+            currentFramerate: newFramerate,
+            lastAnimationTimestamp: time,  
+          }            
+        }
+      );
+    } else {
+      this.setState(
+        (state) => {
+          const timeDifference = time - state.lastAnimationTimestamp;
+          const newFramerate = Math.round(1000/timeDifference);
+          return {
+            ...state,
+            animationRequestID: requestAnimationFrame(this.animationCallback),
+            currentFramerate: newFramerate,
+            lastAnimationTimestamp: time,
+          };
+        }
+      );
+    }
   }
 
   componentDidMount() {
@@ -65,17 +87,24 @@ class MapStage extends Component {
   }
 
   render() {
+    const {
+      left,
+      right,
+      up,
+      down
+    } = this.props.keys;
+    
     const text = `Current framerate: ${this.state.currentFramerate} fps`;
     return (
       <div>
         <Stage width={900} height={600}>
-          <TilingSprite image={tileImg} width={900} height={600} key="tile" />
+          <TilingSprite texture={PIXI.Texture.fromImage(tileImg)} width={900} height={600} key="tile" />
           <Text text={text} x={10} y={10} key="2" />
-          <Text text={`${this.props.keys.up ? 'up' : ''}`} x={10} y={40} anchor={new PIXI.Point(0,0)} key="3" />
-          <Text text={`${this.props.keys.down ? 'down' : ''}`} x={10} y={70} anchor={new PIXI.Point(0,0)} key="4" />
-          <Text text={`${this.props.keys.left ? 'left' : ''}`} x={10} y={100} anchor={new PIXI.Point(0,0)} key="5" />
-          <Text text={`${this.props.keys.right ? 'right' : ''}`} x={10} y={130} anchor={new PIXI.Point(0,0)} key="6" />
-          <Sprite image={robotImg} scale={0.5} x={this.state.robot.x} y={this.state.robot.y} anchor={new PIXI.Point(0.5,0.5)} key="robot" />
+          <Text text={`${up ? 'up' : ''}`} x={10} y={40} anchor={new PIXI.Point(0,0)} key="3" />
+          <Text text={`${down ? 'down' : ''}`} x={10} y={70} anchor={new PIXI.Point(0,0)} key="4" />
+          <Text text={`${left ? 'left' : ''}`} x={10} y={100} anchor={new PIXI.Point(0,0)} key="5" />
+          <Text text={`${right ? 'right' : ''}`} x={10} y={130} anchor={new PIXI.Point(0,0)} key="6" />
+          <Sprite texture={PIXI.Texture.fromImage(robotImg)} scale={0.5} x={this.state.robot.x} y={this.state.robot.y} anchor={new PIXI.Point(0.5,0.5)} key="robot" />
         </Stage>
       </div>
     );
